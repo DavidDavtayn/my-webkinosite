c33443fd6a7e6a1ff7847bec4035db56a3d112c8
Davit <daviddavtyna850@gmail.com>
2024-11-29 16:05:37 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Kinoswiper from "./components/Kinoswiper";
+import Filmi from "./components/Filmi";
+import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import "./App.css";
+
+function Home() {
+    return (
+        <>
+            <Kinoswiper />
+            <Filmi />
+        </>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about-us" element={<AboutUs />} />
+                    <Route path="/contact-us" element={<ContactUs />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
